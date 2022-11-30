8ef016df1c20c73927b7bd43595ee6ad3893225a
João Henrique Alves dos Santos <jaozao77@gmail.com>
2022-11-30 19:59:54 -0300

    Lê arquivo de Log

--- main.py
+++ main.py
@@ -4,14 +4,7 @@ import json
 
 connection = psycopg2.connect("dbname='postgres' user='postgres' password='postgres' host='localhost' port='5432'")
 cursor = connection.cursor()
-
-
-def create_table():
-    cursor.execute("CREATE TABLE IF NOT EXISTS redo (id INTEGER, A INTEGER, B INTEGER)")
-    connection.commit()
-
-
-create_table()
+cursor.execute("CREATE TABLE IF NOT EXISTS redo (id INTEGER, A INTEGER, B INTEGER)")
 
 with open('metadado.json') as f:
     data = json.load(f)
@@ -28,4 +21,20 @@ for i in range(len(data['INITIAL']['id'])):
     print(values)
 
 connection.commit()
+
+
+log = []
+arq = open('entradaLog.txt','r')
+texto = arq.readlines()
+chars = ['<','>','(',')','\n']
+for linha in texto:
+    for char in chars:
+        linha = linha.replace(char, '')
+    linha = linha.replace(' ', ',')
+    linha = linha.split(',')
+    log.append(linha)
+log.pop(len(log)-1)
+for i in log:
+    print(i)
+arq.close()
 connection.close()
