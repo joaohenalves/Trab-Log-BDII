9aab38b95ffc2d3a6782e8f65b9cbeb7a1109631
CataratasDolly <bernardobf2010@hotmail.com>
2022-12-04 23:51:00 -0300

    Comandos

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import json
 
 connection = psycopg2.connect("dbname='postgres' user='postgres' password='postgres' host='localhost' port='5432'")
 cursor = connection.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS redo (id INTEGER, A INTEGER, B INTEGER)")
+cursor.execute("DROP TABLE redo; CREATE TABLE redo (id INTEGER, A INTEGER, B INTEGER)")
 
 with open('metadado.json') as f:
     data = json.load(f)
 
 for i in range(len(data['INITIAL']['id'])):
     values = [data['INITIAL'][item][i] for item in data['INITIAL']]
-    query = "INSERT INTO redo VALUES (%s, %s, %s) "
+    query = "INSERT INTO redo VALUES (%s, %s, %s)"
     cursor.execute(query, tuple(values))
 connection.commit()
 
@@ -30,4 +30,20 @@ log.pop(len(log)-1)
 for i in log:
     print(i)
 arq.close()
+
+transactions, commandQuery = [], []
+for command in log:
+    match command[0]:
+        case "start":
+            transactions.append(command[1])
+        case "commit":
+            pass
+        case "CKPT":
+            pass
+        case "crash":
+            pass
+        case _:
+            if (len(command)) == 5:
+                pass
+
 connection.close()
