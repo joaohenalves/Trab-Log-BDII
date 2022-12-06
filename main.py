d44e7b97543e4f4dde404edaf0fdd5d1975ea234
João Henrique Alves dos Santos <jaozao77@gmail.com>
2022-12-06 13:06:27 -0300

    Trab. finalizado

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import psycopg2
 import json
 
-
 class Transaction:
     def __init__(self, nome):
         self.nome, self.comandos, self.comitada = nome, [], False
 
-
 connection = psycopg2.connect("dbname='postgres' user='postgres' password='postgres' host='localhost' port='5432'")
 cursor = connection.cursor()
-cursor.execute("DROP TABLE IF EXISTS redo; CREATE TABLE redo (id INTEGER, A INTEGER, B INTEGER)")
+cursor.execute("DROP TABLE IF EXISTS redo; CREATE TABLE redo (id INTEGER PRIMARY KEY, A INTEGER, B INTEGER)")
 
 with open('metadado.json') as file:
     data = json.load(file)
@@ -20,14 +18,18 @@ connection.commit()
 transactions, tAux = [], []
 with open('entradaLog.txt', 'r') as file:
     for linha in file:
-        linha = linha.replace('<', '').replace('>', '').replace('(', '').replace(')', '').replace('\n', '').replace(' ', ',').split(',')
+        for char in ['<', '>', '(', ')', '\n']:
+            linha = linha.replace(char, '')
+        linha = linha.replace(' ', ',').split(',')
 
         match linha[0]:
             case "start":
                 transactions.append(Transaction(linha[1]))
             case "commit":
                 for j in range(len(transactions)):
-                    transactions[j].comitada = transactions[j].nome == linha[1]
+                    if transactions[j].nome == linha[1]:
+                        transactions[j].comitada = True
+                        break
             case "CKPT":
                 tAux.extend(k for k in transactions if k.nome in linha[1:])
                 transactions = tAux
@@ -49,8 +51,9 @@ for t in transactions:
             if v[0] != (valor := t.comandos[i][3]):
                 cursor.execute(f"UPDATE redo SET {t.comandos[i][1]} = {valor} WHERE id = {t.comandos[i][0]}")
                 connection.commit()
-    print(f"{t.nome}{'' if t.comitada else ' não'} realizou REDO")
+                print(f"A transação {t.nome} alterou a coluna {t.comandos[i][1]} do id {t.comandos[i][0]} para o valor: {t.comandos[i][3]}")
+    print(f"Transação {t.nome}{'' if t.comitada else ' não'} realizou REDO")
 
-cursor.execute("SELECT * from redo")
+cursor.execute("SELECT * FROM redo ORDER BY id")
 print(cursor.fetchall())
 connection.close()
