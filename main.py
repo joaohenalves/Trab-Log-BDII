e9ae6e7939f8d9600da068180a741c3f44759b8b
CataratasDolly <bernardobf2010@hotmail.com>
2022-12-06 01:12:52 -0300

    ref

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import json
 
 class Transaction:
     def __init__(self, nome):
-        self.nome = nome
-        self.comandos = []
-        self.comitada = False
+        self.nome, self.comandos, self.comitada = nome, [], False
 
 
 connection = psycopg2.connect("dbname='postgres' user='postgres' password='postgres' host='localhost' port='5432'")
@@ -17,26 +15,21 @@ with open('metadado.json') as file:
     data = json.load(file)
 for i in range(len(data['INITIAL']['id'])):
     cursor.execute("INSERT INTO redo VALUES (%s, %s, %s)", tuple(data['INITIAL'][item][i] for item in data['INITIAL']))
-
 connection.commit()
 
 transactions, tAux = [], []
 with open('entradaLog.txt', 'r') as file:
     for linha in file:
-        for char in ['<', '>', '(', ')', '\n']:
-            linha = linha.replace(char, '')
-        linha = linha.replace(' ', ',').split(',')
+        linha = linha.replace('<', '').replace('>', '').replace('(', '').replace(')', '').replace('\n', '').replace(' ', ',').split(',')
 
         match linha[0]:
             case "start":
                 transactions.append(Transaction(linha[1]))
             case "commit":
                 for j in range(len(transactions)):
-                    if transactions[j].nome == linha[1]:
-                        transactions[j].comitada = True
+                    transactions[j].comitada = transactions[j].nome == linha[1]
             case "CKPT":
-                del linha[0]
-                tAux.extend(k for k in transactions if k.nome in linha)
+                tAux.extend(k for k in transactions if k.nome in linha[1:])
                 transactions = tAux
             case "abort":
                 for l in range(len(transactions)):
@@ -44,12 +37,8 @@ with open('entradaLog.txt', 'r') as file:
                         del transactions[l]
                         break
             case _:
-                if (len(linha)) == 5:
-                    for transaction in transactions:
-                        if transaction.nome == linha[0]:
-                            del linha[0]
-                            transaction.comandos.append(linha)
-                            break
+                if len(linha) == 5:
+                    [t.comandos.append(linha[1:]) for t in transactions if t.nome == linha[0]]
 
 for t in transactions:
     if t.comitada:
@@ -59,13 +48,9 @@ for t in transactions:
 
             if v[0] != (valor := t.comandos[i][3]):
                 cursor.execute(f"UPDATE redo SET {t.comandos[i][1]} = {valor} WHERE id = {t.comandos[i][0]}")
-
                 connection.commit()
-        print(f"{t.nome} realizou REDO")
-    else:
-        print(f"{t.nome} não realizou REDO")
+    print(f"{t.nome}{'' if t.comitada else ' não'} realizou REDO")
 
 cursor.execute("SELECT * from redo")
 print(cursor.fetchall())
-
 connection.close()
